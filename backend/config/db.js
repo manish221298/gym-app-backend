95eafe74bb31a442679e42c5f81dc481fb347e59
manish221298 <iammnsh01@gmail.com>
2024-05-02 17:26:23 +0530

    add candidate using form builder and list

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,5 @@
 const mongoose = require("mongoose");
 
-// command to start monngod:  sudo systemctl start mongod
-//
-
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
